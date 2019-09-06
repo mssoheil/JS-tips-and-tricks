a70cc3b50ff8e0e369480ea7f558b844d11723bb
mssoheil <mssoheil88@yahoo.com>
2019-09-06 11:31:32 +0430

    feat: ex4 parseInt weired trick

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const ex1 = require("./ex1/ex1");
 const ex2 = require("./ex2/ex2");
 const ex3 = require("./ex3/ex3");
+const ex4 = require("./ex4/ex4");
 
 // ex1();
 // ex2();
-ex3();
+// ex3();
+ex4();
---
+++ ex4/ex4.js
@@ -0,0 +1,6 @@
+module.exports = function () {
+
+	const data = parseInt(1/0, 19);
+	console.log(data);
+
+}

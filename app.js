4938e8afed0f4cb94a6131ef28a618f4fc2235ba
mssoheil88 <mssoheil88@yahoo.com>
2019-09-07 09:49:44 +0430

    feat: ex5 abstract equality of string and boolean

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const ex1 = require("./ex1/ex1");
 const ex2 = require("./ex2/ex2");
 const ex3 = require("./ex3/ex3");
 const ex4 = require("./ex4/ex4");
+const ex5 = require("./ex5/ex5");
 
 // ex1();
 // ex2();
 // ex3();
-ex4();
+// ex4();
+ex5();
---
+++ ex5/ex5.js
@@ -0,0 +1,7 @@
+module.exports = function() {
+	const x = "56";
+	const y = false;
+	const z = true;
+	console.log(x == y);
+	console.log(x == z);
+};

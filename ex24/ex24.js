10c178e487d1e04356584e4788575b5006f63829
mssoheil88 <mssoheil88@yahoo.com>
2019-09-28 11:29:06 +0330

    feat: ex24 padstart

---
+++ ex24/ex24.js
@@ -0,0 +1,10 @@
+const name = "Lydia Hallie";
+console.log(name.padStart(13));
+console.log(name.padStart(2));
+
+/**
+ * A: "Lydia Hallie", "Lydia Hallie"
+ * B: "[13x whitespace]Lydia Hallie", "[2x whitespace]Lydia Hallie"
+ * C: "[1x whitespace]Lydia Hallie", "Lydia Hallie"
+ * D: "Lydia Hallie", "Lyd"
+ */

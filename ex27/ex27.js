6377b2830442b1f76bfc86d98ff851222cefef71
mssoheil88 <mssoheil88@yahoo.com>
2019-09-30 10:22:35 +0330

    feat: ex27 object referece equality

---
+++ ex27/ex27.js
@@ -0,0 +1,17 @@
+function compareMembers(person1, person2 = person) {
+	if (person1 !== person2) {
+		console.log("Not the same!");
+	} else {
+		console.log("They are the same!");
+	}
+}
+
+const person = { name: "Lydia" };
+
+compareMembers(person);
+/**
+ * A: Not the same!
+ * B: They are the same!
+ * C: ReferenceError
+ * D: SyntaxError
+ */
